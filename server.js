8febce30771c58b4282fc980d15fc393a13c7e1b
Balan Mac <BalanMac@Balans-MacBook-Pro.local>
2019-02-06 20:10:54 +0530

    Initial Commit

---
+++ server.js
@@ -0,0 +1,71 @@
+const express = require('Express');
+const hbs = require('hbs');
+const fs = require('fs');
+
+var app = express();
+
+hbs.registerPartials(__dirname + '/views/partials');
+app.set('view engine', 'hbs');
+
+
+app.use((req, res, next) => {
+  var now = new Date().toString();
+  console.log(`${now}: ${req.method} ${req.path}`);
+  var log = `${now}: ${req.method} ${req.path}`
+
+  fs.appendFile('server.log', log + '/n', (err) => {
+    if(err){
+      console.log("Unalbe to write to file");
+    }
+  })
+  next();
+})
+
+hbs.registerHelper('maint', () => {
+  return 'Maintenance Page Hello';
+})
+
+// app.use((req, res) => {
+//   res.render('maintenance.hbs', {
+//     pageName: "Maintenance"             //"Maintenance Page"
+//   });
+// })
+
+app.use(express.static(__dirname + '/public'));
+
+
+hbs.registerHelper('getCurrentYear', () => {
+    return new Date().getFullYear();
+})
+
+hbs.registerHelper('screamIt', (text) => {
+  return text.toUpperCase();
+})
+app.get('/', (req, res) => {
+  // res.send("Hello Express!");
+
+  res.render('home.hbs', {
+    welcomeTitle: "Welcome to my Website",
+    pageTitle: "Welcome",
+    page: "Home Page",
+
+  })
+});
+
+app.get('/about', (req, res) => {
+  res.render('about.hbs', {
+    pageTitle: "About Title",
+
+  });
+})
+
+app.get('/bad', (req, res) => {
+  res.send({
+    Title: "Bad Request",
+    Description : "Page Not Found"
+  });
+});
+
+app.listen(3000), () => {
+  console.log("Server is up on port 3000");
+};
